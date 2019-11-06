422a642e4918aa13d554374ac2a605ffe3a3b89f
Alisa Song <alisasong@gmail.com>
2019-11-06 15:37:02 -0800

    Add header component, add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 // import { WelcomeComponent } from './welcome/welcome.component';
 // import { LoginComponent } from './components/login/login.component';
 // import { UserChartComponent } from './components/userchart/userchart.component';
 // import { SliceDetailsComponent } from './components/userchart/slice-details.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
