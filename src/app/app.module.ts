e9a8b9688ea4164eda393d92f984b324ecc9d3d8
Alisa Song <alisasong@gmail.com>
2019-11-07 09:52:19 -0800

    Add components for admissions, programs, staff and tour pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { StaffComponent } from './components/staff/staff.component';
+import { TourComponent } from './components/tour/tour.component';
+import { AdmissionsComponent } from './components/admissions/admissions.component';
+import { ProgramsComponent } from './components/programs/programs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    StaffComponent,
+    TourComponent,
+    AdmissionsComponent,
+    ProgramsComponent
   ],
   imports: [
     BrowserModule,
