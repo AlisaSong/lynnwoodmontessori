46e86b3696440bf938d500f8f8db82d99fa38100
Alisa Song <alisasong@gmail.com>
2019-12-04 18:00:10 -0800

    Add programs content, add decorative icons with wrapping hidden.

--- src/app/components/programs/programs.component.ts
+++ src/app/components/programs/programs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BrowserUtil } from 'src/app/utils/browser-util';
+import { faSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-programs',
@@ -7,11 +8,14 @@ import { BrowserUtil } from 'src/app/utils/browser-util';
   styleUrls: ['./programs.component.scss']
 })
 export class ProgramsComponent implements OnInit {
+  faSquare = faSquare;
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   constructor(private browserUtil: BrowserUtil) { }
 
   ngOnInit() {
     this.browserUtil.resetScroll();
+    console.log(window.innerWidth);
   }
 
 }
