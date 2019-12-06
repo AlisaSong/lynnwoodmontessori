0ad42da02461086fa2fe153515b64be512405a35
Alisa Song <alisasong@gmail.com>
2019-12-06 13:53:15 -0800

    Update header styling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, Event, NavigationStart, NavigationEnd, NavigationError  } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 import { faBars, faWindowClose } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
