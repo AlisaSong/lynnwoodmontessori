126109cf5615dbdf4775ca99357c31d7ae22529d
Alisa Song <alisasong@gmail.com>
2019-12-06 19:50:47 -0800

    fix iframe issues

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BrowserUtil } from 'src/app/utils/browser-util';
+import { Router } from '@angular/router';
 import { faSquare, faCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -12,7 +13,7 @@ export class HomeComponent implements OnInit {
   faCircle = faCircle;
   numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  constructor(private browserUtil: BrowserUtil) { }
+  constructor(private browserUtil: BrowserUtil, private router: Router) { }
 
   ngOnInit() {
     this.browserUtil.resetScroll();
--- src/app/components/tour/tour.component.ts
+++ src/app/components/tour/tour.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BrowserUtil } from 'src/app/utils/browser-util';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tour',
@@ -7,11 +8,15 @@ import { BrowserUtil } from 'src/app/utils/browser-util';
   styleUrls: ['./tour.component.scss']
 })
 export class TourComponent implements OnInit {
+  url: string = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19502.16684984935!2d-122.35095995244247!3d47.84594984131133!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549004e69011cc33%3A0xebae3ea955ac2eb7!2s6009%20168th%20St%20SW%2C%20Lynnwood%2C%20WA%2098037!5e0!3m2!1sen!2sus!4v1575671759733!5m2!1sen!2sus";
+  safeSrc: SafeResourceUrl;
 
-  constructor(private browserUtil: BrowserUtil) { }
+  constructor(private browserUtil: BrowserUtil, public sanitizer: DomSanitizer) {
+   }
 
   ngOnInit() {
     this.browserUtil.resetScroll();
-  }
+    this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
 
+  }
 }
