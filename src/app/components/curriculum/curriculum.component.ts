26244604c5bac4d4d34c408941e3981974596af8
Alisa Song <alisasong@gmail.com>
2019-12-13 00:28:29 -0800

    Add curriculum component and content.

--- src/app/components/curriculum/curriculum.component.ts
+++ src/app/components/curriculum/curriculum.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BrowserUtil } from 'src/app/utils/browser-util';
 import { Router } from '@angular/router';
+import { faSquare, faCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-curriculum',
@@ -8,7 +9,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./curriculum.component.scss']
 })
 export class CurriculumComponent implements OnInit {
-
+  faSquare = faSquare;
+  faCircle = faCircle;
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
   constructor(private browserUtil: BrowserUtil, private router: Router) { }
 
   ngOnInit() {
